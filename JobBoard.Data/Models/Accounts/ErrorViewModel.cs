fa87c2451b76c3b9c31f80304e82894e610d3d47
mSoftBg <mario.pavlov.pavlov@gmail.com>
2018-02-27 14:03:23 +0200

    Initial Refactoring

---
+++ JobBoard.Data/Models/Accounts/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JobBoard.Data.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
